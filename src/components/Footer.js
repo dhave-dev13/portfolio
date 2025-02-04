d6300f306f496e254d007919cdb311ba8b5f5e0c
dhave-wfa <dhave@webfuelagency.com>
2025-02-04 08:07:48 +0800

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
+          <h3>Credits to <a href="https://github.com/soumyajit4419/Portfolio" className="purple">Soumyajit Behera</a> for the template</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
+          <h3>Copyright © {year} DB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
