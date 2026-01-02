4ab47509dd2b96b21a6ddb9e157d552d60d80a5f
dhave-dev13 <81951340+dhave-dev13@users.noreply.github.com>
2026-01-03 02:32:10 +0800

    Update ResumeNew.js
    Added new link for Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Dhave-Barsalote-Resume.pdf";
+import pdf from "../../Assets/../Assets/My Resume - 01-2026.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
